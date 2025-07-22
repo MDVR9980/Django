b5586e5367521ebbb91b039c37fca7d2fd93d383
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-07-22 11:00:41 +0330

    update

--- shop/courses_app/urls.py
+++ shop/courses_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list', views.course_list)
+    path('list', views.course_list),
+    path('detail/<int:id>', views.course_detail)
 ]
