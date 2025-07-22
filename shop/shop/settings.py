fb03b87ad1b7797e8dadbf4730b78394c79c78dc
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-07-22 12:59:50 +0330

    update

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -1,3 +1,4 @@
+from os import path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -108,6 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    path.join(BASE_DIR, 'assets')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
