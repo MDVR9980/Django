4cabe89b982846a23e372f6dfb89b2623a3600f0
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-07-24 00:58:22 +0330

    update

--- personal/personal/settings.py
+++ personal/personal/settings.py
@@ -111,14 +111,14 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
-
+USE_TZ = True 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 # Default primary key field type
--- personal/projects_app/admin.py
+++ personal/projects_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from projects_app.models import Project
 
-# Register your models here.
+admin.site.register(Project)
