8da7e0cf7f38af820ac751f18edaf1127c6e4421
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-07-26 13:35:28 +0330

    update

--- blog/post/models.py
+++ blog/post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Meny (article) To One (user):
 # each acticle has only one author
@@ -10,17 +11,19 @@ from django.contrib.auth.models import User
 # set default
 # protect
 # do nothing
+
 class Article(models.Model):
     # author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     # author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default='1')
     # author = models.ForeignKey(User, on_delete=models.PROTECT)
     # author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=70)
+    title = models.CharField(max_length=70, unique_for_date='pub_date')
     body = models.TextField()
     image = models.ImageField(upload_to="images/articles")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    pub_date = models.DateField(default=timezone.now)
 
     def __str__(self):
         return f"{self.title} - {self.body[:30]}"
