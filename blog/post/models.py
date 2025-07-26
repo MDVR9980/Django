3d0e60fb0af05f2c1f71c9a270a071a9a9fe00e0
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-07-26 22:22:29 +0330

    update

--- blog/post/models.py
+++ blog/post/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
 # Meny (article) To One (user):
 # each acticle has only one author
 # each user can has several article
@@ -18,6 +26,7 @@ class Article(models.Model):
     # author = models.ForeignKey(User, on_delete=models.PROTECT)
     # author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=70, unique_for_date='pub_date')
     body = models.TextField()
     image = models.ImageField(upload_to="images/articles")
