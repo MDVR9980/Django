75ae0522e18a22f29876d38641a6b06d37e3972d
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-07-26 13:07:05 +0330

    update

--- blog/post/models.py
+++ blog/post/models.py
@@ -4,9 +4,18 @@ from django.contrib.auth.models import User
 # Meny (article) To One (user):
 # each acticle has only one author
 # each user can has several article
-
+# USER -> DELETE
+# cascade
+# set null
+# set default
+# protect
+# do nothing
 class Article(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, )
+    # author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    # author = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default='1')
+    # author = models.ForeignKey(User, on_delete=models.PROTECT)
+    # author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=70)
     body = models.TextField()
     image = models.ImageField(upload_to="images/articles")
