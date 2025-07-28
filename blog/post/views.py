1e66470473f7d0ea709eb7ed559b0d8c538ae244
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-07-28 10:12:54 +0330

    update

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from post.models import Article
 
-# Create your views here.
+def post_detail(request, pk):
+    article = Article.objects.get(id=pk)
+    return render(request, 'post/post_detail', {'article': article})
