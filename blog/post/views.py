25da9b11e883030f85e56185ecbf7c459f5a6e71
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-07-28 11:09:42 +0330

    update

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from post.models import Article
 
 def post_detail(request, pk):
-    article = Article.objects.get(id=pk)
-    return render(request, 'post/post_detail', {'article': article})
+    # article = Article.objects.get(id=pk)
+    article = get_object_or_404(Article, id=pk)
+    return render(request, 'post/post_detail.html', {'article': article})
