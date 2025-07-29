0844bf0537a0d6024e7562dcdfcc65eaee15c3cc
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-07-29 09:56:43 +0330

    update

---
+++ blog/context_processors/context_processors.py
@@ -0,0 +1,6 @@
+from post.models import Article
+
+def recent_articles(request):
+    recent_articles = Article.objects.order_by('-created')
+
+    return {"recent_articles": recent_articles}
--- blog/home/views.py
+++ blog/home/views.py
@@ -3,9 +3,9 @@ from post.models import Article
 
 def home(request):
     articles = Article.objects.all()
-    recent_articles = Article.objects.all().order_by('-created')
+    # recent_articles = Article.objects.all().order_by('-created')
 
     # articles = Article.objects.published() 
     # print(Article.objects.counter())
     
-    return render(request, "home/index.html", {'articles': articles, 'recent_articles': recent_articles})
+    return render(request, "home/index.html", {'articles': articles})
